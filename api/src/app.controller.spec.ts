eb74024c3e1b4e5f55294c97405622680ca85841
Johan Bell <belljohan3@gmail.com>
2026-04-24 07:11:02 +0100

    fix test

--- api/src/app.controller.spec.ts
+++ api/src/app.controller.spec.ts
@@ -1,7 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmailService } from './email/email.service';
 import { PrismaService } from './prisma/prisma.service';
+import { ObjectStorageService } from './storage/object-storage.service';
 
 describe('AppController', () => {
     let appController: AppController;
@@ -17,6 +19,18 @@ describe('AppController', () => {
                         $queryRaw: jest
                             .fn()
                             .mockResolvedValue([{ '?column?': 1 }]),
+                    },
+                },
+                {
+                    provide: ObjectStorageService,
+                    useValue: {
+                        isConfigured: jest.fn().mockReturnValue(false),
+                    },
+                },
+                {
+                    provide: EmailService,
+                    useValue: {
+                        isConfigured: jest.fn().mockReturnValue(false),
                     },
                 },
             ],
